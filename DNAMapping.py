7e59f1a7b3e3db51a517484886c2d0e333b9655b
Chad <chadsbacon@gmail.com>
2016-11-14 15:28:35 -0700

    Created an initial stub with basic program flow.

---
+++ DNAMapping.py
@@ -0,0 +1,15 @@
+import sys
+
+
+def map_dna(input_file):
+    # Parse input
+
+    # Build Suffix Tree
+
+    # Map DNA sequence
+
+    # Output results
+
+
+if __name__ == "__main__":
+    map_dna(sys.argv)
