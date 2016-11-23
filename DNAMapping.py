c0180010830766ab6bea834ddc770e6bab4b7277
Kevin Johansen <MrSkyBear@gmail.com>
2016-11-22 22:18:54 -0700

    Implemented skipping in read mapping if matches are found in original sequence.

--- DNAMapping.py
+++ DNAMapping.py
@@ -2,6 +2,8 @@ import sys
 from SuffixTree import SuffixTree
 from Parser import Parser
 from SAM import SAM
+from datetime import datetime
+
 
 
 def map_dna(args):
@@ -35,84 +37,91 @@ def map_dna(args):
     print("DONE")
 
     read_file_name = read_file.split(".")[0]
-    sam = SAM(filename="%s.SAM" % sequence_name, sequence_length=len(sequence))
+    timestamp = datetime.now().strftime("%Y-%d-%H-%M")
+    file_name = "%s %s.SAM" % (sequence_name, timestamp)
+    sam = SAM(filename=file_name, sequence_name=sequence_name, sequence_length=len(sequence))
 
     reads = parser.parse_fasta_reads(read_file)
 
     # Map DNA sequence
-    print("Beginning Mapping Process")
+    print("**Beginning Mapping Process")
     counter = 0
+    results = []
+    matches = {}
+
     for read_name, read in reads.iteritems():
         counter += 1
+        kmer_indices = {}
+        print("**Mapping Read " + str(counter) + " ")
+        max_index = -1
+        max_score = -1
+        candidate_mapping_indices = {}
 
-        print("Mapping Read " + str(counter) + " ")
+        kmer_start_index = 0
+        kmer_end_index = kmer_start_index + k
 
-        candidate_index = find_dna_mapping(suffix_array, bwt, first_occurrences, counts, read, k)
+        while kmer_end_index - 1 <= len(read):
+            kmer = read[kmer_start_index:kmer_end_index]
+            relative_index = kmer_start_index
 
-        print(sam.append_sam_output(read_name=read_name, sequence_name=sequence_name,
-                                    position=candidate_index+1, read=read))
+            if kmer not in matches:
+                matches[kmer] = find_pattern_matches(kmer, suffix_array, bwt, first_occurrences, counts)
 
 
-def find_dna_mapping(suffix_array, bwt, first_occurrences, counts, read, k):
+            if matches[kmer]:
 
-    def find_pattern_matches(kmer):
-        indices = []
+                # Perform pattern matching using suffix_tree with each kmer storing results for each matching kmer index relative
+                # to its location within the read, i.e. matching_index - relative_index = potential_read_mapping_index
+                for matching_index in matches[kmer]:
+                    potential_read_index = matching_index - relative_index
 
-        top = 0
-        bottom = len(bwt) - 1
-        kmer_index = len(kmer) - 1
+                    if potential_read_index >= 0:
+                        if potential_read_index not in candidate_mapping_indices:
+                            candidate_mapping_indices[potential_read_index] = 0
 
-        while top <= bottom:
-            # print top, bottom
-            if kmer_index >= 0:
-                symbol = kmer[kmer_index]
-                kmer_index -= 1
+                        candidate_mapping_indices[potential_read_index] += 1
 
-                if symbol in bwt[top:bottom + 1]:
-                    top = first_occurrences[symbol] + counts[symbol][top]
-                    bottom = first_occurrences[symbol] + counts[symbol][bottom + 1] - 1
-                else:
-                    break
+                        if candidate_mapping_indices[potential_read_index] > max_score:
+                            max_index = potential_read_index
+                            max_score = candidate_mapping_indices[potential_read_index]
+
+                kmer_start_index += k
+                kmer_end_index += k
+
             else:
-                for i in xrange(top, bottom + 1):
-                    indices.append(suffix_array[i])
-                break
+                kmer_start_index += 1
+                kmer_end_index += 1
 
-        return indices
 
-    # Prepare list of kmers. Map each kmer with its position(s) within the read.
-    kmer_indices = {}
-    
-    for i in xrange(len(read) - k + 1):
-        kmer = read[i:i + k]
-        
-        if kmer not in kmer_indices:
-            kmer_indices[kmer] = []
+        sam.append_sam_output(read_name=read_name, cigar="%dM" % len(read), sequence_name=sequence_name, position=max_index+1)
             
-        kmer_indices[kmer].append(i)
-        
-    # Perform pattern matching using suffix_tree with each kmer storing results for each matching kmer index relative
-    # to its location within the read, i.e. matching_index - relative_index = potential_read_mapping_index
-    candidate_mapping_indices = {}
-    max_index = -1
-    max_score = -1
-    for kmer, relative_indices in kmer_indices.iteritems():
-        matching_indices = find_pattern_matches(kmer)
 
-        for matching_index in matching_indices:
-            for relative_index in relative_indices:
-                potential_read_index = matching_index - relative_index
+    print("**MAPPING COMPLETE**")
 
-                if potential_read_index not in candidate_mapping_indices:
-                    candidate_mapping_indices[potential_read_index] = 0
+def find_pattern_matches(kmer, suffix_array, bwt, first_occurrences, counts):
+    indices = []
 
-                candidate_mapping_indices[potential_read_index] += 1
+    top = 0
+    bottom = len(bwt) - 1
+    kmer_index = len(kmer) - 1
 
-                if candidate_mapping_indices[potential_read_index] > max_score:
-                    max_index = potential_read_index
-                    max_score = candidate_mapping_indices[potential_read_index]
+    while top <= bottom:
+        # print top, bottom
+        if kmer_index >= 0:
+            symbol = kmer[kmer_index]
+            kmer_index -= 1
 
-    return max_index
+            if symbol in bwt[top:bottom + 1]:
+                top = first_occurrences[symbol] + counts[symbol][top]
+                bottom = first_occurrences[symbol] + counts[symbol][bottom + 1] - 1
+            else:
+                break
+        else:
+            for i in xrange(top, bottom + 1):
+                indices.append(suffix_array[i])
+            break
+
+    return indices
 
 
 def suffix_array_from_suffix_tree(suffix_tree):
--- SAM.py
+++ SAM.py
@@ -1,21 +1,21 @@
 class SAM:
 
-    def __init__(self, filename, sequence_length):
+    def __init__(self, filename, sequence_name, sequence_length):
         self.filename = filename
 
-        header = "@HD\tVN:1.0\tSO:unknown\n@SQ\tSN:%s\tLN:%s" % (filename.split(".")[0], sequence_length)
+        header = "@HD\tVN:1.0\tSO:unknown\n@SQ\tSN:%s\tLN:%s" % (sequence_name, sequence_length)
         with open(filename, 'w') as f:
             f.write(header)
 
 
-    def append_sam_output(self, read_name, sequence_name, position, read):
+    def append_sam_output(self, read_name, cigar, sequence_name, position):
         flag = 0
         mapping_quality = 255
-        cigar = "%dM" % len(read)
         rnext = "*"
         pnext = 0
         tlen = 0
         quality = "*"
+        read = "*"
 
         result = [read_name, flag, sequence_name, position, mapping_quality, cigar, rnext, pnext, tlen, read, quality]
         result_string = "\t".join([str(x) for x in result])
@@ -24,3 +24,4 @@ class SAM:
             f.write("\n%s" % result_string)
 
         return result_string
+
